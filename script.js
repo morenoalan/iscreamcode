c7735c05c4adc16a0fc8dd69e205149a9aaf2abb
Alan Moreno <alan.moreno@hotmail.com>
2022-11-13 22:43:22 -0300

    start commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function Save(){
+    console.log('Save button');
+}
+
+function Clear(){
+    console.log('Clear button');
+}
+
+function View(){
+    console.log('View button');
+}
+
+function DraggingBar(element){
+    console.log(element.id +' tab');
+}
+
+function BoxSettings(element){
+    console.log(element.innerText +' button');
+}
